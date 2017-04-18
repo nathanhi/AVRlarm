34b409a708add47a51a085ee62a2e43874df3d01
Nathan-J. Hirschauer <nathanhi@deepserve.info>
2017-04-18 20:51:05 +0200

    Silence compiler warning Werror=return-local-addr

--- src/main.c
+++ src/main.c
@@ -19,9 +19,10 @@ void print_info() {
 }
 
 int freeMem() {
-  extern int __heap_start, *__brkval;
-  int v;
-  return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval);
+    extern int __heap_start, *__brkval;
+    int v;
+    int i = (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval);
+    return i;
 }
 #endif
 
